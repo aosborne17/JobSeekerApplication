4278ee06d3baf18a3285fceaec25f1ae82943e31
Andrew <andrew@conducttr.com>
2021-01-10 13:13:00 +0000

    Adding frontend files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
-    <div>
-        <h1>Job Seeker Application</h1>
-    </div>
-  )
-}
+    <Router>
+      <div className='app'>
+        <Header />
+        <div className='app__body'>
+          <Route exact path='/login' component={LoginScreen} />
+          <Route exact path='/' component={HomeScreen} />
+          {/* <Route exact path='/product/:id' component={ProductScreen} />
+          <Route exact path='/cart/:id?' component={CartScreen} /> */}
+        </div>
+      </div>
+    </Router>
+  );
+};
 
-export default App
-
+export default App;
